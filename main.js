436b5d91610b1211e57ae150ca7930f9dbca00f4
Strencher <46447572+Strencher@users.noreply.github.com>
2022-02-28 13:56:13 +0100

    Fix CSP issue + new settings

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
 const {app} = require("electron");
-const {config: {MAX_ATTEMPTS}} = require("./index.json");
-
+const {config: {MAX_ATTEMPTS, DELETE_HEADERS, APPEND_HEADERS}} = require("./index.json");
 let sentryRegex = /ignoreErrors.*BetterDiscord/si;
 let sentryURL = null;
+
+const showConsoleLog = (tries, win) => process.nextTick(() => {
+    win.webContents.executeJavaScript(`(() => {
+        console.log("[SentryBlock] Blocked sentry", {
+            url: ${JSON.stringify(sentryURL)},
+            attempts: ${JSON.stringify(tries)}
+        });
+    })()`);
+    console.log("[SentryBlock] Blocked sentry", {
+        url: sentryURL,
+        attempts: tries
+    });
+});
+
 app.whenReady().then(() => {
     app.on("browser-window-created", (_, win) => {
         let tries = 0;
         let queue = new Set();
         win.webContents.session.webRequest.onHeadersReceived((opts, callback) => {
-            if (sentryURL) return callback({cancel: sentryURL === opts.url});
-            if (queue.has(opts.url)) return callback({});
-            if (tries > MAX_ATTEMPTS) return callback({});
-            if (opts.resourceType !== "script") return callback({});
-            if (opts.url.indexOf("discord.com/assets") < 0) return callback({});
+            const {responseHeaders} = opts;
+            for (const header in APPEND_HEADERS) {
+                responseHeaders[header] ??= APPEND_HEADERS[header];
+            }
+
+            for (const header of DELETE_HEADERS) {
+                if (responseHeaders[header]) delete responseHeaders[header];
+            }
+
+            if (sentryURL) {
+                const isSentry = sentryURL === opts.url;
+                if (isSentry) showConsoleLog(tries, win);
+                
+                return callback({cancel: isSentry, responseHeaders});
+            }
+
+            if (queue.has(opts.url)) return callback({responseHeaders});
+            if (tries > MAX_ATTEMPTS) return callback({responseHeaders});
+            if (opts.resourceType !== "script") return callback({responseHeaders});
+            if (opts.url.indexOf("discord.com/assets") < 0) return callback({responseHeaders});
             
             require("https").get(opts.url, (res) => {
                 const data = [];
                 res.on("data", d => data.push(d));
                 res.on("end", () => {
-                    if (sentryURL) return callback({});
+                    if (sentryURL) return callback({responseHeaders});
                     const body = data.join("");
                     queue.delete(opts.url);
 
                     const isSentry = sentryRegex.test(body);
                     if (isSentry) {
                         sentryURL = opts.url;
-                        callback({cancel: true});
+                        showConsoleLog(tries, win);
+                        callback({cancel: true, responseHeaders});
                         queue.clear();
-                        process.nextTick(() => {
-                            win.webContents.executeJavaScript(`(() => {
-                                console.log("[SentryBlock] Found sentry", {
-                                    url: ${JSON.stringify(opts.url)},
-                                    attempts: ${JSON.stringify(tries)}
-                                });
-                            })()`);
-                            console.log("[SentryBlock] Found sentry", {
-                                url: opts.url,
-                                attempts: tries
-                            });
-                        });
+                        
                     } else {
-                        callback({});
+                        callback({responseHeaders});
                     }
                 });
             });
