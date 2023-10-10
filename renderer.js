9e2da06fe481307f8e3ccf739d901626ddefb7b5
Mario <eternal.git@gmail.com>
2023-10-10 20:31:57 +0100

    even faster

--- renderer.js
+++ renderer.js
@@ -1,47 +1,115 @@
 async function init() {
-   await new Promise(resolve => {
-      if (window.webpackChunkdiscord_app?.push) {
-         resolve();
-      }
+	predefine(window, 'webpackChunkdiscord_app', instance => {
+		predefine(instance, 'push', orig => {
+			const data = {
+				blocked: {
+					sdk: false,
+					logger: false,
+					core: false,
+					discord: false,
+					client: false,
+					stack: false
+				}
+			};
 
-      Object.defineProperty(window, "webpackChunkdiscord_app", {
-         get: () => undefined,
-         configurable: true,
-         set: (value) => {
-            delete window.webpackChunkdiscord_app;
-            window.webpackChunkdiscord_app = value;
-            resolve();
-         }
-      });
-   });
+			instance.push = function (payload) {
+				if (data.blocked.logger && data.blocked.sdk && data.blocked.core && data.blocked.discord && data.blocked.stack && data.blocked.client) {
+					return orig.call(this, payload);
+				}
 
-   const data = {
-      push: window.webpackChunkdiscord_app.push,
-      blocked: false,
-      tries: 0,
-   };
+				const chunk = payload[1];
 
-   Object.defineProperty(window.webpackChunkdiscord_app, "push", {
-      set: (value) => {data.push = value;},
-      get: () => function (payload) {
-         if (data.blocked) {
-            return data.push.apply(this, [payload]);
-         }
+				for (const [_id, mdl] of Object.entries(chunk)) {
+					const id = Number(_id);
 
-         if (data.tries < 5) {
-            const chunk = payload[1];
-            data.tries++;
+					if (!data.blocked.sdk && ~mdl.toString().indexOf('__SENTRY__')) {
+						chunk[id] = () => { };
+						console.log('[Sentry] Blocked SDK.');
+						data.blocked.sdk = true;
+						continue;
+					}
 
-            if (Object.values(chunk).find(m => ~m.toString().indexOf("BetterDiscord"))) {
-               return data.blocked = true;
-            }
-         }
+					if (!data.blocked.client && ~mdl.toString().indexOf('_sdkProcessingMetadata')) {
+						chunk[id] = () => { };
+						console.log('[Sentry] Blocked client.');
+						data.blocked.client = true;
+						continue;
+					}
 
-         return data.push.apply(this, [payload]);
-      }
-   });
+					if (!data.blocked.core && ~mdl.toString().indexOf('makeXHRTransport') && ~mdl.toString().indexOf('addBreadcrumb')) {
+						chunk[id] = () => { };
+						console.log('[Sentry] Blocked Core.');
+						data.blocked.core = true;
+					}
+
+					if (!data.blocked.stack && ~mdl.toString().indexOf('_sendSessionUpdate')) {
+						chunk[id] = () => { };
+						console.log('[Sentry] Blocked stack.');
+						data.blocked.stack = true;
+					}
+
+					if (!data.blocked.discord && ~mdl.toString().indexOf('DiscordSentry')) {
+						chunk[id] = () => { };
+						console.log('[Sentry] Blocked Discord global.');
+						data.blocked.discord = true;
+					}
+
+					if (!data.blocked.logger && ~mdl.toString().indexOf('Sentry Logger')) {
+						chunk[id] = () => { };
+						console.log('[Sentry] Blocked logger.');
+						data.blocked.logger = true;
+					}
+				}
+
+				payload[1][712343] = () => { };
+				return orig.call(this, payload);
+			};
+
+			instance.push([[Symbol()], {}, require => {
+				require.d = (target, exports) => {
+					for (const key in exports) {
+						if (!Reflect.has(exports, key) || target[key]) continue;
+
+						Object.defineProperty(target, key, {
+							get: () => exports[key](),
+							set: v => exports[key] = () => v,
+							enumerable: true,
+							configurable: true
+						});
+					}
+				};
+			}]);
+
+			instance.pop();
+		});
+	});
 }
 
+function predefine(target, prop, effect) {
+	const value = target[prop];
+
+	Object.defineProperty(target, prop, {
+		get: () => value,
+		set: (newValue) => {
+			Object.defineProperty(target, prop, {
+				value: newValue,
+				configurable: true,
+				enumerable: true,
+				writable: true
+			});
+
+			try {
+				effect(newValue);
+			} catch (error) {
+				console.error(error);
+			}
+
+			return newValue;
+		},
+		configurable: true
+	});
+};
+
 if (window.__NO_SENTRY__?.config?.EXPERIMENTAL_SENTRY_BLOCK) {
-   init();
+	init();
 }
