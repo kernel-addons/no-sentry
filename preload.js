d000289bb04255b7a8b7a1bfeb179a521ac2528f
Mario <eternal.git@gmail.com>
2023-10-10 18:37:55 +0100

    hello instant startup ( ͡° ͜ʖ ͡°)

--- preload.js
+++ preload.js
@@ -4,3 +4,17 @@ const {config} = require("./index.json");
 if (config?.EXPERIMENTAL_SENTRY_BLOCK ?? false) {
    contextBridge.exposeInMainWorld("__NO_SENTRY__", { config });
 }
+
+const oLoad = Module._load;
+const fakeSentry = {
+	init: () => { },
+	captureException: console.error,
+	setUser: () => void 0,
+	setTag: () => void 0
+};
+
+Module._load = function (mod) {
+	if (mod.indexOf("sentry") > -1) return fakeSentry;
+
+	return oLoad.apply(this, arguments);
+};
